335653d5c863a58e8c338b5ef72a2faa5a7d9da7
Somnath Chakraborty <61010000+somnath352@users.noreply.github.com>
2021-05-21 23:34:12 +0530

    First commit

---
+++ chocolate_monger.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int t;
+	cin >> t;
+	while(t--){
+		int n, x;
+		cin >> n >> x;
+		set<int> st;
+		int a[n];
+		for(int i = 0; i < n; ++i){
+			cin >> a[i];
+			st.insert(a[i]);
+		}
+		int m = st.size();
+		if(n - m >= x){   
+			cout << m << endl;
+		}
+		else{          
+			int var = n - m;
+			var = x - var;
+			cout << m - var<< endl;
+		}
+	}
+}
+
